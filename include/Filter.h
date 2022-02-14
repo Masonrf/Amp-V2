7b16e5c24df4be58b14f3020884566c4ce496b1a
Masonrf <masonrfleck@gmail.com>
2022-02-13 22:03:20 -0600

    Sample rate is 48kHz not 4.8kHz...

--- include/Filter.h
+++ include/Filter.h
@@ -5,32 +5,36 @@
  */
 
 
-// A-weighting filter coeffs (AWF)
-// http://gaidi.ca/weblog/implementing-a-biquad-cascade-iir-filter-on-a-cortex-m4#
+/*  A-weighting filter coeffs (AWF)
+ *  http://gaidi.ca/weblog/implementing-a-biquad-cascade-iir-filter-on-a-cortex-m4#
+ *  This is a Direct Form II Transposed at Fs = 48kHz
+ */
+
 #define AWF_IIR_ORDER       6
 #define AWF_IIR_NUM_STAGES  (AWF_IIR_ORDER/2)
 
 static float32_t m_biquad_state[AWF_IIR_ORDER];
 static float32_t m_biquad_coeffs[5 * AWF_IIR_NUM_STAGES] = {
     1.0000000000f,
-    0.0000000000f,
-    -1.0000000000f,
-    -0.4286284394f,
-    0.2710337076f,
+    2.0000000000f,
     1.0000000000f,
-    0.0000000000f,
-    -1.0000000000f,
-    0.0910482487f,
-    0.6749836143f,
+    0.2245584581f,
+    -0.0126066253f,
     1.0000000000f,
     -2.0000000000f,
     1.0000000000f,
-    1.9467893387f,
-    -0.9474971824f
+    1.8938704947f,
+    -0.8951597691f,
+    1.0000000000f,
+    -2.0000000000f,
+    1.0000000000f,
+    1.9946144560f,
+    -0.9946217070f
 };
-// I can use this if I want, but IDRC about gain > 1 in this case, so I probably won't
+// I can use this if I want, but IDRC about gain > 1 in this case
+// 0th element is gain before the first biquad
 static float32_t m_biquad_scale[AWF_IIR_NUM_STAGES + 1] = {
-    0.6094088340f,
+    0.2341830434f,
     1.0000000000f,
     1.0000000000f,
     1.0000000000f
